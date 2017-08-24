4f450ccde095805c5cfac9557bcf6286cb52371b
prabhanjan_k_nag <chatbotnag@gmail.com>
2017-08-24 01:33:55 -0700

    refactored

--- Slink/gulpfile.js
+++ Slink/gulpfile.js
@@ -4,15 +4,26 @@
  * and open the template in the editor.
  */
 
-var gulp = require('gulp');
-var browser=require('browser-sync');
+var fs=require('fs');
+var gulp=require('gulp');
 
-gulp.task('default', function () {
-    // place code for your default task here
+var path=require('path');
+
+var browserSync=require('browser-sync');
+
+gulp.task('reload',function(){
+    browserSync.reload();
 });
 
+gulp.task('watch',function(){
+    gulp.watch('./src/*.js',['reload']);
+   });
 
-gulp.task('serve',function(){
-	//look at the watch function
-	browser.reload();
+gulp.task('serve',['watch'],function(){
+    browserSync({
+        server:{
+            baseDir: "./",
+            index:"./index.html"
+        }
+    });
 });
---
+++ Slink/main.js
@@ -0,0 +1,11 @@
+
+requirejs.config({
+    baseUrl: './'
+    
+});
+require([],function(){
+    alert('RequireJS');
+    console.log('RequireJS');
+});
+
+
